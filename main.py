8ce3eb4c4188acd6e27741fb0186735cceeeae4f
Maxime Gaudin <mgaudin@Maximes-MacBook-Pro.local>
2014-03-21 01:41:19 +0100

    Add POST and GET features

---
+++ main.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python 
+
+from bottle import route, run, template, post, HTTPResponse, request, get
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+client = MongoClient() 
+db = client.apimorph
+
+def content_to_response(content):
+  a = [record for record in content]
+  for r in a:
+    r['_id'] = r['_id']
+
+  return dumps({
+    "links": [],
+    "meta": {
+      "page": 1,
+      "size": 20,
+      "total": content.count()
+      },
+    "content": a
+    })
+
+
+@post('/<resource>')
+def post_handler(resource):
+  resource_id = db[resource].insert(request.json)
+  return HTTPResponse(status=201, 
+      headers={
+        "Content-Type": "application/json; charset=utf8",
+        "Location": "/users/{0}".format(resource_id)
+        }, 
+      body=resource)
+
+
+@get('/<resource>')
+def list_handler(resource):
+  content = db[resource].find()
+  return HTTPResponse(status=200, 
+      headers={
+        "Content-Type": "application/json; charset=utf8"
+        }, body=content_to_response(content))
+
+
+@get('/<resource>/<id>')
+def list_handler(resource, id):
+  content = db[resource].find_one(id)
+  return HTTPResponse(status=200, body=content_to_response(content))
+
+run(host='localhost', port=8091, reloader=True, debug=True)
